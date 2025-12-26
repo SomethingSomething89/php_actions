686d56febc17e12489b3e2ab35f8d57360871686
SomethingSomething89 <somethingnull935@gmail.com>
2025-12-26 18:20:08 +0200

    Fixed config,php

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
-
-$env = parse_ini_file(__DIR__ . '/.env');
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
 
 $pdo = new PDO(
-    "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8mb4",
-    $env['DB_USER'],
-    $env['DB_PASSWORD']
+    "mysql:host=$host;dbname=$db;charset=utf8mb4",
+    $user,
+    $pass,
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
