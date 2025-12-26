9b3b0613c4c160d3ca4a754a065df5c4c2e28a2f
SomethingSomething89 <somethingnull935@gmail.com>
2025-12-26 18:11:25 +0200

    Fixed config,php

--- config.php
+++ config.php
@@ -2,13 +2,8 @@
 
 $env = parse_ini_file(__DIR__ . '/.env');
 
-$conn = new mysqli(
-    $env['DB_HOST'],
+$pdo = new PDO(
+    "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8mb4",
     $env['DB_USER'],
-    $env['DB_PASSWORD'],
-    $env['DB_NAME']
+    $env['DB_PASSWORD']
 );
-
-if ($conn->connect_error) {
-    die("Database connection failed");
-}
